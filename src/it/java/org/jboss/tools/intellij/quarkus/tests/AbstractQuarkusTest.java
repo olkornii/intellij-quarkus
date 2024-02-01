593626cfd121480ae4e52777e5d339f727039419
Oleksii Korniienko <olkornii@redhat.com>
2024-02-01 11:57:04 +0100

    Added workaround for IJ-2022.3
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- src/it/java/org/jboss/tools/intellij/quarkus/tests/AbstractQuarkusTest.java
+++ src/it/java/org/jboss/tools/intellij/quarkus/tests/AbstractQuarkusTest.java
@@ -11,14 +11,23 @@
 package org.jboss.tools.intellij.quarkus.tests;
 
 import com.intellij.remoterobot.RemoteRobot;
+import com.intellij.remoterobot.fixtures.HeavyWeightWindowFixture;
+import com.intellij.remoterobot.fixtures.JLabelFixture;
+import com.intellij.remoterobot.fixtures.JListFixture;
+import com.intellij.remoterobot.fixtures.JTreeFixture;
 import com.redhat.devtools.intellij.commonuitest.UITestRunner;
 import com.redhat.devtools.intellij.commonuitest.fixtures.dialogs.FlatWelcomeFrame;
+import com.redhat.devtools.intellij.commonuitest.fixtures.dialogs.information.TipDialog;
+import com.redhat.devtools.intellij.commonuitest.utils.constants.XPathDefinitions;
+import com.redhat.devtools.intellij.commonuitest.utils.internalerror.IdeInternalErrorUtils;
 import com.redhat.devtools.intellij.commonuitest.utils.runner.IntelliJVersion;
 import com.redhat.devtools.intellij.commonuitest.utils.testextension.ScreenshotAfterTestFailExtension;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.time.Duration;
+
+import static com.intellij.remoterobot.search.locators.Locators.byXpath;
 
 /**
  * Abstract test class
@@ -39,9 +48,33 @@ abstract public class AbstractQuarkusTest {
 
             FlatWelcomeFrame flatWelcomeFrame = remoteRobot.find(FlatWelcomeFrame.class, Duration.ofSeconds(10));
             flatWelcomeFrame.disableNotifications();
-            flatWelcomeFrame.preventTipDialogFromOpening();
+//            flatWelcomeFrame.preventTipDialogFromOpening();
+            preventTipDialogFromOpening();
+            flatWelcomeFrame.switchToProjectsPage();
         }
     }
+
+    public static void preventTipDialogFromOpening() {
+        IdeInternalErrorUtils.clearWindowsErrorsIfTheyAppear(remoteRobot);
+        JTreeFixture jTreeFixture = remoteRobot.find(JTreeFixture.class, byXpath(XPathDefinitions.TREE));
+        jTreeFixture.findText("Learn").click();
+        FlatWelcomeFrame flatWelcomeFrame = remoteRobot.find(FlatWelcomeFrame.class);
+        flatWelcomeFrame.findText("Tip of the Day").click();
+
+        TipDialog tipDialog = remoteRobot.find(TipDialog.class, Duration.ofSeconds(10));
+        tipDialog.dontShowTipsCheckBox().setValue(true);
+        tipDialog.close();
+    }
+
+//    public TipDialog MyopenTipDialog() {
+//        IdeInternalErrorUtils.clearWindowsErrorsIfTheyAppear(remoteRobot);
+//        JTreeFixture jTreeFixture = remoteRobot.find(JTreeFixture.class, byXpath(XPathDefinitions.TREE));
+//        jTreeFixture.findText("Learn").click();
+//        FlatWelcomeFrame flatWelcomeFrame = remoteRobot.find(FlatWelcomeFrame.class);
+//        flatWelcomeFrame.findText("Tip of the Day").click();
+//
+//        return remoteRobot.find(TipDialog.class, Duration.ofSeconds(10));
+//    }
 
     private static class CloseIntelliJBeforeQuit extends Thread {
         @Override
